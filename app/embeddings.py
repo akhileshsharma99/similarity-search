62216f409112b4fadfc9d10036f93ede75f1157a
Akhilesh Sharma <Akhilesh.Sharma@neudesic.com>
2023-08-30 16:17:58 -0400

    changed sentence transformer cache path

--- app/embeddings.py
+++ app/embeddings.py
@@ -1,8 +1,11 @@
 from langchain.embeddings import HuggingFaceEmbeddings
 from typing import List
+import os
 
 def embed_text(text) -> List[float]:
+    cache_folder_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'cache')
     embeddings = HuggingFaceEmbeddings(
-        model_name="all-MiniLM-L6-v2", cache_folder="./cache")
+        model_name="all-MiniLM-L6-v2", cache_folder=cache_folder_path)
     data = embeddings.embed_query(text)
     return data
+
